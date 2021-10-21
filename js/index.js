99562d6a18ec0e457cdb4d55191206bc12e5e480
mascettig <mascettigj@gmail.com>
2021-10-21 18:58:51 -0300

    tercera preentrega

--- js/index.js
+++ js/index.js
@@ -41,3 +41,29 @@ function validarFormulario(e){
     console.log(formulario.children[5].value);
     console.log(formulario.children[6].value);
 }
+const productos = [ { id: 1, nombre: "Zapatillas", precio: 12500},
+{ id: 2, nombre:"Zapatillas claras", precio: 14500},
+{ id: 3, nombre: "Zapatillas azules", precio: 12000 },
+{ id: 4, nombre: "Zapatillas marrones", precio: 8500}];
+
+for (const producto of productos)(
+  $("#app"). append(`<div><h3> ID: ${productos.id}</h3>
+  <p> Pdoductos: ${productos.nombre}</p>
+  <b> $ ${productos.precio} </b><div>`)
+
+const URLGET =
+  "https://jsonplacerholder.typicode.com/posts"
+  $("body").prepend(`<button id="btn1">GET>/button>`);
+  $("#btn1").click( () => {
+    $.get(URLGET, function (respuesta, estado) {
+      if(estado === "success"){
+        let misDatos = respuestas;
+        for (const dato of misDatos) {
+          $("body").prepend (`<div>
+                              <h3> $(dato.title)</h3>
+                              <p>$(dato.body)</p>
+                              </div>`);
+        }
+      }
+    })
+  });
